16ddbcf412fe5c599c8f43faf3d9a44e2643351a
tal <talolard@gmail.com>
2014-12-12 12:02:13 +0200

    Fixed the mobile input bug

--- conference/forms.py
+++ conference/forms.py
@@ -6,9 +6,10 @@ class NameForm(forms.Form):
     Caller = forms.CharField(label='Enter Your phone number (eg +14156634567)\n', max_length=100)
 
 class PhoneNumberForm(forms.Form):
-    CountryCode = forms.IntegerField(label="Country Code",widget=forms.TextInput(attrs={'size': '3','class':'test'} ),initial=1)
-    AreaCode = forms.IntegerField(label="Area Code",widget=forms.TextInput(attrs={'size': '3'}),initial=800)
-    PhoneNumber = forms.IntegerField(label="Phone Number",widget=forms.TextInput(attrs={'size': '10'}),initial=5555555)
+
+    CountryCode = forms.IntegerField(label="Country Code",widget=forms.NumberInput(attrs={'size': '3','class':'test'} ))
+    AreaCode = forms.IntegerField(label="Area Code",widget=forms.NumberInput(attrs={'size': '3'}))
+    PhoneNumber = forms.IntegerField(label="Phone Number",widget=forms.NumberInput(attrs={'size': '10'}))
     def get_number(self):
         return "+{0}{1}{2}".format(self.cleaned_data["CountryCode"],
                                    self.cleaned_data["AreaCode"],
